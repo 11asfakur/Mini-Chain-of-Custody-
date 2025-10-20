107716dfb3c7b60169fdbb52285434d3e8daaf74
11asfakur <asfakurr@utas.edu.au>
2025-10-21 04:47:52 +1100

    updated firebase config

---
+++ js/firebase-config.js
@@ -0,0 +1,18 @@
+// Firebase Configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAlemnsNkn6OewEqsF5FLwa1JxQRvrkukA",
+  authDomain: "mini-chain-of-custody.firebaseapp.com",
+  projectId: "mini-chain-of-custody",
+  storageBucket: "mini-chain-of-custody.firebasestorage.app",
+  messagingSenderId: "1091266900897",
+  appId: "1:1091266900897:web:d07054b06c0a2108fa91e0"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+// Initialize services
+const auth = firebase.auth();
+const db = firebase.firestore();
+const storage = firebase.storage();
+
+console.log('🔥 Firebase initialized successfully!');
